1e032dc94387141356542923e300e7f44de48286
olayiwolaA <mike.ow994@gmail.com>
2022-08-25 15:10:32 -0700

    program that imports the function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-def main():
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    
-    print(f"{a} + {b} = {add}")
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
