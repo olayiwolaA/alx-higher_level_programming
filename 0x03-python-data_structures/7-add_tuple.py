68fe90d555e6307be257642cc51cbae4e292ed23
olayiwolaA <mike.ow994@gmail.com>
2022-08-29 14:18:37 -0700

    adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2:
+        addtup1 = tuple_a[:2]
+    else:
+        if len(tuple_a) == 1:
+            addtup1 = tuple_a[0], 0
+        else:
+            addtup1 = 0, 0
+    if len(tuple_b) >= 2:
+        addtup2 = tuple_b[:2]
+    else:
+        if len(tuple_b) == 1:
+            addtup2 = tuple_b[0], 0
+        else:
+            addtup = 0, 0
+    return (addtup1[0] + addtup2[0], addtup1[1] + addtup2[1])
