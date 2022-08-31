060ff877c6f142c5382629becaff3673b5a19566
olayiwolaA <mike.ow994@gmail.com>
2022-08-31 15:33:59 -0700

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,8 +7,8 @@ def roman_to_int(roman_string):
         roman_str = 0
         for rom in range(len(rom_str)):
             if rom > 0 and (rom_num[rom_str[rom]] > rom_num[rom_str[rom - 1]]):
-                roman_str += rom_num[rom_str[rom]] -
-                (2 * rom_num[roman_string[rom - 1]])
+                roman_str += rom_num[rom_str[rom]]
+                - (2 * rom_num[roman_string[rom - 1]])
             else:
                 roman_str += rom_num[rom_str[rom]]
         return roman_str
