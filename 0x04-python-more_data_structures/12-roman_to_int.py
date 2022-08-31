289ae529343105f5d1414a7da3e714c7078d3872
olayiwolaA <mike.ow994@gmail.com>
2022-08-31 15:38:22 -0700

    Technical interview preparation

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
+
+
 def roman_to_int(roman_string):
     if roman_string and isinstance(roman_string, str):
         rom_num = {'I': 1, 'V': 5, 'X': 10, 'L':
