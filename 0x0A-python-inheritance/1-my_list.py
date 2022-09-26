9ab79c6cb86e0880c85bd775ff34268adc516ec6
olayiwolaA <mike.ow994@gmail.com>
2022-09-26 05:29:35 -0700

    Adding Files for tasks

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+   1-my_list module
+   Class MyList that inherits from list that contain \
+   Public instance method: def print_sorted(self): that \
+   prints the list, but sorted (ascending sort)
+"""
+
+
 class MyList(list):
     """ Class that inherits the attributes references of class list
 
