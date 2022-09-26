f3890a90c5d4127d357b3522148c6c390844cc91
olayiwolaA <mike.ow994@gmail.com>
2022-09-26 14:34:15 -0700

    Adding Files for tasks

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -3,19 +3,21 @@
 
 
 class BaseGeometry:
-    """Class BaseGeometry"""
+    """Reprsent base geometry."""
 
     def area(self):
-        """Public instance method: def area(self): \
-           that raises an Exception with\
-           the message area() is not implemented.
-        """
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Public instance method:\
-           def integer_validator(self, name, value):\
-           that validates value"""
+        """Validate a parameter as an integer.
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
