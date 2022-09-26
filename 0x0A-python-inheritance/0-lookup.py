784fdb2a260501bdeff09161a460de0c375c6d0e
olayiwolaA <mike.ow994@gmail.com>
2022-09-26 05:26:58 -0700

    Adding Files for Tasks

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    0-lookup module
+    Search available attributes and methods of an object
+"""
+
+
 def lookup(obj):
     """ Function that returns the list of available attributes
         and methods of an object
