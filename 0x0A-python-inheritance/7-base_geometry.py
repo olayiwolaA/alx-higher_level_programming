a6b87850fbcfbb3bf80f6d5e141bdb781d383646
olayiwolaA <mike.ow994@gmail.com>
2022-09-26 14:25:50 -0700

    Adding files for Tasks

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,23 +12,19 @@
 
 
 class BaseGeometry:
-    """ Class that defines the attributes of Geometric Shapes """
+    """Class BaseGeometry"""
 
     def area(self):
-        """ Method that defines the area of a geomtric shape """
-
+        """Public instance method: def area(self): \
+           that raises an Exception with\
+           the message area() is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Method that recieves the value property
-
-        Árgs:
-            name: name of the object
-            value: value of the property
-
-        """
-
-        if type(value) is not int:
+        """Public instance method:\
+           def integer_validator(self, name, value):\
+           that validates value"""
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
