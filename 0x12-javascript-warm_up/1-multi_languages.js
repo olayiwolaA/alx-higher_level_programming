96939c3f4ff2a6db5277291a91e6b0efd740a891
olayiwolaA <mike.ow994@gmail.com>
2022-11-29 13:18:52 -0800

    Files updated

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let i = 0;
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
